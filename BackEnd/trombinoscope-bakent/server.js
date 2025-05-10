535c67cf63dc7a93286e7150ea72bfc0844bd3d4
ATTOMOYE Jonathan <126904964+JonaKaileur@users.noreply.github.com>
2025-05-10 11:14:50 -0300

    ajout back end 3

---
+++ BackEnd/trombinoscope-bakent/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(()=>{
+    console.log("bdd connecté");
+  })
+  .catch((err) => {
+    console.log("erreur de co a bdd !!!");
+  })
+
+
+  const membreShema = new mongoose.Schema({
+    nom: String,
+    prenom: String,
+    mail: String,
+    poste: String,
+
+  });
+
+  const Membres = mongoose.model('Membre',membreShema);
+
+  app.get('/membres',function(req,res){
+    Membres.find({},function(err,membres){
+      if (err) throw err;
+      res.json(membres);
+    })
+  })
+
+
+  app.post('/membres',function(req,res){
+    const nouveauMembre = new Membres(req.body);
+    nouveauMembre.save();
+    res.json(nouveauMembre);
+  })
+
+
